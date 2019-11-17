1eb74e1f507d2d9b451c63d393f775dc0ea56ea7
juicy0429 <paran700@gmail.com>
2019-11-17 11:38:44 +0900

    no message

--- app/src/main/java/com/example/nearspot/InformationActivity.java
+++ app/src/main/java/com/example/nearspot/InformationActivity.java
@@ -1,8 +1,8 @@
 package com.example.nearspot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class InformationActivity extends AppCompatActivity {
 

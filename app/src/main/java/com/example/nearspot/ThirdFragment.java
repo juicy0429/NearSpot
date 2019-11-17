6ea09e9fcfe8f39cdc82392b5f9a92fa07f09301
juicy0429 <paran700@gmail.com>
2019-11-17 11:38:26 +0900

    fragment 작업

---
+++ app/src/main/java/com/example/nearspot/ThirdFragment.java
@@ -0,0 +1,29 @@
+package com.example.nearspot;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.fragment.app.Fragment;
+
+public class ThirdFragment extends Fragment {
+    public ThirdFragment()
+    {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.item_store_card, container, false);
+        return layout;
+    }
+}
+
+
